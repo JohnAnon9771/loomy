bdede734a954a7e08d4e13345376adcccfafee9b
João Alves <njoao97710@gmail.com>
2026-03-08 23:52:56 -0300

    refactor: stack to use keyword args and simplify layout logic

--- lib/loomy/ops/stack.rb
+++ lib/loomy/ops/stack.rb
@@ -3,42 +3,43 @@ module Loomy
     class Stack < Base
       attr_reader :direction, :spacing, :align, :valign
 
-      def initialize(direction:, spacing: 0, align: nil, valign: nil, background_width: nil, background_height: nil)
+      def initialize(direction:, spacing: 0, align: nil, valign: nil, **options)
         super(input: nil)
         @direction, @spacing, @align, @valign = direction, spacing, align, valign
-        @compositor = Compositor.new(background_width: background_width, background_height: background_height)
+        @compositor = Compositor.new(**options)
       end
 
-      def add_layer(op, properties = {})
-        @compositor.add_layer(op, properties)
-      end
-      def layers = @compositor.layers
+      def add_layer(...) = @compositor.add_layer(...)
+      def layers         = @compositor.layers
 
       def call(context = nil)
         prepared = @compositor.prepare(context)
         return @compositor.render([]) if prepared.empty?
 
-        layout!(prepared)
-        @compositor.render(prepared)
+        @compositor.render(arrange(prepared))
       end
 
       private
 
-      def layout!(layers)
-        layers.reduce(0) do |offset, layer|
-          size = if direction == :vertical
+      def arrange(layers)
+        offset = 0
+
+        layers.each do |layer|
+          if vertical?
             layer.props[:y]     = offset
             layer.props[:align] ||= align
-            layer.image.height
+
+            offset += layer.image.height + spacing
           else
             layer.props[:x]      = offset
             layer.props[:valign] ||= valign
-            layer.image.width
-          end
 
-          offset + size + spacing
+            offset += layer.image.width + spacing
+          end
         end
       end
+
+      def vertical? = direction == :vertical
     end
   end
 end
